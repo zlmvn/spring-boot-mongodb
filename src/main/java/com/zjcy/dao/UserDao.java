472f67e95a9927c77cd518ae4051a3a28d996c00
flyap <qr1234>
2018-09-10 11:51:39 +0800

    Initial commit

---
+++ src/main/java/com/zjcy/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.zjcy.dao;
+
+
+import com.zjcy.entity.UserEntity;
+
+/**
+ * Created by summer on 2017/5/5.
+ */
+public interface UserDao  {
+
+    public void saveUser(UserEntity user);
+
+    public UserEntity findUserByUserName(String userName);
+
+    public int updateUser(UserEntity user);
+
+    public void deleteUserById(Long id);
+
+}
